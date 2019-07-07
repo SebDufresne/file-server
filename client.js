d52217f86fa1f672d3fbf991de57124ed6c52a1e
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-07 20:01:21 +0000

    First iteration of code, list command implemented

--- client.js
+++ client.js
@@ -1,18 +1,36 @@
 const net = require('net');
+const readline = require('readline');
 
-const conn = net.createConnection({
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const client = net.createConnection({
   host: 'localhost', // change to IP address
   port: 3000
 });
 
+client.setEncoding('utf8'); // interpret data as text
 
-conn.setEncoding('utf8'); // interpret data as text
+client.on('connect', () => {
 
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
+  client.on('data', (data) => {
+    console.log(data);
+    
+    rl.question(`> `, (command) => {
+      client.write(command);
+      rl.close();
+    });
+  });
 });
 
-conn.on('connect', () => {
-  conn.write('Hello from client!');
+//Add event listener for close events
+client.on('close', () => {
+  console.log(`Server disconnected`);
 });
 
+//Add listener for error events
+client.on('error', error => {
+  console.log(`Error : ${error}`);
+});
--- fileServer.js
+++ fileServer.js
@@ -1,15 +1,72 @@
+'use strict;';
+
 const net = require('net');
+const fs = require('fs');
 
 const server = net.createServer();
 
-server.on('connection', (client) => {
-  console.log('New client connected!');
-  client.write('Hello there!');
+const FILE_PATH = './data/';
+const MESSAGES = [
+  `-=-=- Welcome to the File Server -=-=-\n`,
+  `Command list:\n` +
+  `list: list all available files\n` +
+  `get <file_name>: get the content of a file\n` +
+  `What would you like to do?`,
+  'You can retrieve a file with the command get <file_name>',
+  
+];
 
-  client.setEncoding('utf8'); // interpret data as text
-  client.on('data', (data) => {
-    console.log('Message from client: ', data);
+//Store the clients in an object
+const clients = {};
+
+//Declaring an iterator for our clients (should use a randomizer)
+let clientId = 0;
+
+server.on('connection', connection => {
+  //Logging the fact that we have a client & add it to the object
+  clients[clientId] = connection;
+  clientId++;
+
+  console.log(connection.remoteAddress + " : " + connection.remotePort + " just connected.\n");
+
+  connection.setEncoding('utf8'); // interpret data as text
+
+  connection.write(`${MESSAGES[0]}\n${MESSAGES[1]}`);
+
+  connection.on('data', (data) => {
+
+
+    if (/list/.test(data)) {
+      let listOfFiles = [];
+      fs.readdir(FILE_PATH, (error, files) =>  {
+        for (let i = 0; i < files.length; i++) {
+          if (/[.]txt/.test(files[i])) {
+            listOfFiles.push(files[i]);
+          }
+        }
+        listOfFiles.push(MESSAGES[2]);
+        connection.write(listOfFiles.join('\n'));
+      });
+    }
   });
+
+  connection.on('end', () => {
+    for (const key in clients) {
+      if (clients[key] === connection) {
+        delete clients[key];
+      }
+    }
+  });
+});
+
+//Add event listener for close events
+server.on('close', () => {
+  console.log(`Server disconnected`);
+});
+
+//Add listener for error events
+server.on('error', error => {
+  console.log(`Error : ${error}`);
 });
 
 server.listen(3000, () => {
